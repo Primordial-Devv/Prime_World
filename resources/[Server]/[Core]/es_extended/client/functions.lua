33297e871c01d5071b43fc858b2e6d31c626d6cd
Primordial_Stduio <153821912+Primordial-Devv@users.noreply.github.com>
2024-01-17 16:29:34 +0100

    Replace society and extended modify
    Replacing ESX society by my version of society with ox_lib menu
    
    And remove every dependency of ESX context in the server

--- resources/[Server]/[Core]/es_extended/client/functions.lua
+++ resources/[Server]/[Core]/es_extended/client/functions.lua
@@ -143,24 +143,6 @@ ESX.HashString = function(str)
     return input_map
 end
 
-local contextAvailable = GetResourceState("esx_context") ~= "missing"
-
-function ESX.OpenContext(...)
-    return contextAvailable and exports["esx_context"]:Open(...) or not contextAvailable and print("[^1ERROR^7] Tried to ^5open^7 context menu, but ^5esx_context^7 is missing!")
-end
-
-function ESX.PreviewContext(...)
-    return contextAvailable and exports["esx_context"]:Preview(...) or not contextAvailable and print("[^1ERROR^7] Tried to ^5preview^7 context menu, but ^5esx_context^7 is missing!")
-end
-
-function ESX.CloseContext(...)
-    return contextAvailable and exports["esx_context"]:Close(...) or not contextAvailable and print("[^1ERROR^7] Tried to ^5close^7 context menu, but ^5esx_context^7 is missing!")
-end
-
-function ESX.RefreshContext(...)
-    return contextAvailable and exports["esx_context"]:Refresh(...) or not contextAvailable and print("[^1ERROR^7] Tried to ^5Refresh^7 context menu, but ^5esx_context^7 is missing!")
-end
-
 ESX.RegisterInput = function(command_name, label, input_group, key, on_press, on_release)
     RegisterCommand(on_release ~= nil and "+" .. command_name or command_name, on_press)
     Core.Input[command_name] = on_release ~= nil and ESX.HashString("+" .. command_name) or ESX.HashString(command_name)
